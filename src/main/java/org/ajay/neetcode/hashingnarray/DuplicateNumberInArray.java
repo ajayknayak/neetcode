52a96ce5aaa00d340205c3f4338d790388cba9f5
ajay <ajay@localhost>
2024-01-04 20:23:24 +0530

    Source updated

--- src/main/java/org/ajay/neetcode/hashingnarray/DuplicateNumberInArray.java
+++ src/main/java/org/ajay/neetcode/hashingnarray/DuplicateNumberInArray.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * Source: https://leetcode.com/problems/contains-duplicate/
+ * 
  * Given an integer array nums, return true if any value appears at least twice
  * in the array, and return false if every element is distinct.
  * 
